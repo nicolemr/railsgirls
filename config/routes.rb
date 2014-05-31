0f5a19ff69eef2c48cc0a8a11c7c59c2bdfcff08
Nicole Robinson <nicole.robinson@gmail.com>
2014-05-31 17:16:27 -0400

    added user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Railsgirls::Application.routes.draw do
+  resources :users
+
   resources :comments
 
-  get "home/index"
+  get 'home/index'
+
   resources :ideas
 
   root "home#index"
+
+  resources :sessions
+
+  get 'login' => "sessions#new"
+  get 'logout' => "sessions#destroy"
+  # =>"localhost:3000/login"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
